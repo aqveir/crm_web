4874f9cbd8b66032a5416b5a114944bc0f9204f4
EllaiSys <admin@ellaisys.com>
2021-05-26 01:55:30 +0530

    Amit: Feature: Page search (basic) in setting screens (WIP)

--- projects/crmo-backend/src/app/modules/console/widgets/widget-subheader/widget-subheader.component.ts
+++ projects/crmo-backend/src/app/modules/console/widgets/widget-subheader/widget-subheader.component.ts
@@ -1,28 +1,72 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
+//Application files
+import { Globals } from 'projects/crmo-backend/src/app/app.global';
+
+//Application Files
+import { BaseComponent } from '../../../base.component';
+
 @Component({
   selector: 'crmo-backend-widget-subheader',
   templateUrl: './widget-subheader.component.html',
   styleUrls: ['./widget-subheader.component.scss']
 })
-export class WidgetSubheaderComponent implements OnInit {
+export class WidgetSubheaderComponent extends BaseComponent implements OnInit {
   @Input('title') strTitle: string=null;
   @Input('page-info') strPageInformation: string=null;
   @Input('routerLinkAdd') strRouterLinkAddBtn: string|any[]=null;
   @Input('search') boolShowSearchControl: boolean=false;
+  @Input('searchFull') boolShowFullSearchControl: boolean=false;
   @Input('filter') boolShowFilterButton: boolean=false;
   @Input('save') boolShowSaveButton: boolean=false;
   @Input('save-group') boolShowSaveButtonGroup: boolean=false;
   @Input('back') boolShowBackButton: boolean=false;
   @Input('loading') boolShowLoading: boolean=false;
   @Output('back') btnBack: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output('search') strSearch: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  /**
+   * Default constructor
+   */
+  constructor(
+    private _globals: Globals
+  ) {
+    super();
   }
 
+
+  /**
+   * Lifecycle Hook's
+   */
+  ngOnInit(): void {
+
+    //Initilaize component
+    this.fnInitialize();
+  } //Function ends
+
+
+  /**
+   * Initialize
+   */
+  private fnInitialize(): void {
+  } //Function ends
+
+
+  public fnSearch(event): void {
+    try {
+      if (event && event.target) {
+        let strSearch: string =  event.target?.value;
+        if (strSearch) {
+          strSearch = strSearch.trim();
+          this.strSearch.emit(strSearch);
+        } //End if
+      } //End if
+    } catch(error) {
+      throw error;
+    } //try-catch ends
+  } //Function ends
   
+
   /**
    * Back Button Emitter
    */
