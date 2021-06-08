7c786c3dcdc3d479352051f6afad6baed2f9276a
EllaiSys <admin@ellaisys.com>
2021-06-09 01:47:40 +0530

    Amit: Fix: Organization List and Details data.

--- projects/crmo-lib/src/lib/interfaces/organization/organization.interface.ts
+++ projects/crmo-lib/src/lib/interfaces/organization/organization.interface.ts
@@ -1,75 +1,81 @@
-import { IUser } from '../user/user.interface';
 import { IConfiguration } from '../common/configuration.interface';
 import { ILookupValue } from '../common/lookup.interface';
 import { ICountry } from '../common/country.interface';
 
 
 export interface IOrganization {
+    logo: string;
+
     hash: string;
     name: string;
-    logo?: string;    
-    subdomain?: string;
-    website?: string;
-    search_tag?: string;
+    subdomain: string;
+    custom_domain: string;
+    website: string;
 
-    contact_person_name?: string;
-    phone?: string;
-    phone_idd?: string;
-    email?: string;
+    address: string;
+    locality: string;
+    city: string;
+    zipcode: string;
+    google_place_id: string;
+    longitude: number;
+    latitude: number;
 
-    address?: string;
-    locality?: string;
-    city?: string;
-    state_id?: number;
-    country?: ICountry[];
-    zipcode?: string;
-    google_place_id?: string;
-    longitude?: number;
-    latitude?: number;
-
-    timezone?: any;
-    industry?: ILookupValue;
-    configurations?:IConfiguration[];
-
-    users_count: number;
+    contact_person_name: string;
+    email: string;
+    phone: string;
+    phone_idd: string;
+    search_tag: string;
     last_updated_at: Date;
+    is_active: boolean;
+    
+    users_count: number;
+    industry: ILookupValue;
+    state_id: number;
+    country: ICountry[];
+    timezone: any;
+    configurations?:IConfiguration[];
 }
 
 export interface IOrganizationMinimal {
     hash: string;
     name: string;
     subdomain: string;
-    logo: string;
     users_count: number;
     last_updated_at: Date;
+    is_active: boolean;
+    logo: string;
 }
 
 export interface IOrganizationRequest {
-    name: string;
-    logo?: any;    
-    subdomain?: string;
-    website?: string;
-    search_tag?: string;
+    logo: string;
 
-    first_name?: string;
-    last_name?: string;
+    name: string;
+    subdomain: string;
+    custom_domain: string;
+    website: string;
+
+    address: string;
+    locality: string;
+    city: string;
+    zipcode: string;
+    google_place_id: string;
+    longitude: number;
+    latitude: number;
+
     contact_person_name: string;
-    phone?: string;
-    phone_idd?: string;
     email: string;
+    phone: string;
+    phone_idd: string;
+    search_tag: string;
+    last_updated_at: Date;
+    is_active: boolean;
+
+    first_name: string;
+    last_name: string;
 
     industry_key: string;
-    timezone_id?: number;
+    timezone_code: string;
 
-    address?: string;
-    locality?: string;
-    city?: string;
-    state_id?: number;
-    country_id?: number;
-    zipcode?: string;
-    google_place_id?: string;
-    longitude?: number;
-    latitude?: number;
-
-    is_active?: boolean;
+    state_id: number;
+    country_code: string;
 }
