03c0fefd554c64e8d345f576c426242849eb6120
Amit <support@aqveir.in>
2023-09-04 19:06:34 +0530

    fix: update AuthGaurd for ng16

--- projects/crm-backend-web/src/app/app-routing.module.ts
+++ projects/crm-backend-web/src/app/app-routing.module.ts
@@ -1,20 +1,44 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy, mapToCanActivate } from '@angular/router';
 
 //Guard Service
 import { AuthGuardService as AuthGuard } from './guards/auth-guard/auth-guard.service';
 
 const routes: Routes = [
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
-  { path: 'secure', canActivate: [AuthGuard], loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) },
+  { path: 'secure', canActivate: mapToCanActivate([AuthGuard]), loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) },
   { path: 'error', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule) },
   { path: '', pathMatch: 'full', redirectTo: 'user/login' },
   { path: '**', redirectTo: 'error/404' },
 ];
 
+
+@Injectable({providedIn: 'root'})
+export class TemplatePageTitleStrategy extends TitleStrategy {
+
+  /**
+   * Constructor
+   * @param title 
+   */
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(_routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(_routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`My Application | ${title}`);
+    } // End if
+  }
+} //Class ends
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+    providers: [
+      { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+  ]
 })
 export class AppRoutingModule { }
 
